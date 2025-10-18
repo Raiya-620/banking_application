b40f1729db75690ce9db6d93080db2bafcfe5d55
Suraiya <medsusu713@gmail.com>
2025-10-18 14:30:13 +0000

    user registration and login api endpoints, passed all testing criteria

---
+++ backend/users/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # Hash the password
+        user.save()
+        return user
+
+    def get_extra_kwargs(self):
+        return {'password': {'write_only': True}}
